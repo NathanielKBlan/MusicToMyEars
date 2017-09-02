fdd88af9c212283359c994523a54dd12e34f0652
Nathaniel K. Blanquel <nateblanquel@gmail.com>
2017-09-02 13:16:04 -0700

    Updated on 9/2/17
    -Fixed bug in which gradient did not fill the entire background.

--- src/app/home-page/home.component.ts
+++ src/app/home-page/home.component.ts
@@ -29,7 +29,12 @@ import {Component} from '@angular/core'
 
     `,
   styles: [`
-    html, body{
+    html{
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
       height: 100%;
       width: 100%;
       margin: 0;
